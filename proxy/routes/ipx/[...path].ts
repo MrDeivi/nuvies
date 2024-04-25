54b87b4bb856490dac76269cead0be031346f092
MrDeivi <dalfonsogcia@gmail.com>
2024-04-25 17:39:23 -0400

    fix: added rate limit to proxy
    Signed-off-by: MrDeivi <dalfonsogcia@gmail.com>

--- proxy/routes/ipx/[...path].ts
+++ proxy/routes/ipx/[...path].ts
@@ -1,21 +1,45 @@
 import { createIPX, createIPXMiddleware } from 'ipx'
 
 const ipx = createIPX({
-  maxAge: 3600,
-  alias: {
-    '/tmdb': 'https://image.tmdb.org/t/p/original/',
-    '/youtube': 'https://img.youtube.com/',
-  },
-  domains: [
-    'image.tmdb.org',
-    'img.youtube.com',
-  ],
+	maxAge: 3600,
+	alias: {
+		'/tmdb': 'https://image.tmdb.org/t/p/original/',
+		'/youtube': 'https://img.youtube.com/',
+	},
+	domains: [
+		'image.tmdb.org',
+		'img.youtube.com',
+	],
 })
 
 const ipxMiddleware = createIPXMiddleware(ipx)
 const ipxHandler = fromNodeMiddleware(ipxMiddleware)
 
-export default eventHandler((event) => {
-  event.node.req.url = `/${event.context.params!.path}`
-  return ipxHandler(event)
+
+import { Ipware } from '@fullerstack/nax-ipware'
+import { Ratelimit } from '@upstash/ratelimit'
+import { kv } from '@vercel/kv'
+
+const ipware = new Ipware();
+const ratelimit = new Ratelimit({
+	redis: kv,
+	// 5 requests from the same IP in 10 seconds
+	limiter: Ratelimit.slidingWindow(15, '30 s'),
+})
+
+export default eventHandler(async (event) => {
+	const ip = ipware.getClientIP(event.node.req)
+
+	//   const ip = event.node.req.socket.remoteAddress ?? event.node.req.headers['x-forwarded-for'] ?? '127.0.0.1'
+
+	if (!ip?.ip) return
+
+	const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+		ip.ip
+	)
+	
+	if(!success) return
+
+	event.node.req.url = `/${event.context.params!.path}`
+	return ipxHandler(event)
 })
