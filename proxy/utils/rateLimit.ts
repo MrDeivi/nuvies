49000eb085fb2c93b8af76284f8109f6dfda116e
MrDeivi <dalfonsogcia@gmail.com>
2024-04-29 23:06:37 -0400

    fix: updated rate limit
    Signed-off-by: MrDeivi <dalfonsogcia@gmail.com>

--- proxy/utils/rateLimit.ts
+++ proxy/utils/rateLimit.ts
@@ -6,8 +6,8 @@ const { Ipware } = naxIpware
 const ipware = new Ipware()
 const ratelimit = new Ratelimit({
   redis: kv,
-  // 100 requests from the same IP in 1 day
-  limiter: Ratelimit.slidingWindow(100, '86400 s'),
+  // 500 requests from the same IP in 1 hour
+  limiter: Ratelimit.slidingWindow(500, '3600 s'),
 })
 
 export async function rateLimitRequest(req: any) {
