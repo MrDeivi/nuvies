44cbdf3b3d49e08f6bd6e4cd60fe13f2968a28f7
MrDeivi <dalfonsogcia@gmail.com>
2024-07-16 09:18:42 -0400

    added log
    Signed-off-by: MrDeivi <dalfonsogcia@gmail.com>

--- proxy/routes/tmdb/[...path].ts
+++ proxy/routes/tmdb/[...path].ts
@@ -3,33 +3,35 @@ import { rateLimitRequest } from '../../utils/rateLimit'
 const TMDB_API_URL = 'https://api.themoviedb.org/3'
 
 export default defineEventHandler(async (event) => {
-  const { success, error } = await rateLimitRequest(event.node.req)
+	const { success, error } = await rateLimitRequest(event.node.req)
 
-  if (!success)
-    return error
+	if (!success) {
+		console.log('Invalid request ');
+		return error
+	}
 
-  const query = getQuery(event)
-  const config = useRuntimeConfig()
-  if (!config.tmdb.apiKey)
-    throw new Error('TMDB API key is not set')
-  try {
-    return await $fetch(event.context.params!.path, {
-      baseURL: TMDB_API_URL,
-      params: {
-        api_key: config.tmdb.apiKey,
-        language: 'en-US',
-        ...query,
-      },
-      headers: {
-        Accept: 'application/json',
-      },
-    })
-  }
-  catch (e: any) {
-    const status = e?.response?.status || 500
-    setResponseStatus(event, status)
-    return {
-      error: String(e)?.replace(config.tmdb.apiKey, '***'),
-    }
-  }
+	const query = getQuery(event)
+	const config = useRuntimeConfig()
+	if (!config.tmdb.apiKey)
+		throw new Error('TMDB API key is not set')
+	try {
+		return await $fetch(event.context.params!.path, {
+			baseURL: TMDB_API_URL,
+			params: {
+				api_key: config.tmdb.apiKey,
+				language: 'en-US',
+				...query,
+			},
+			headers: {
+				Accept: 'application/json',
+			},
+		})
+	}
+	catch (e: any) {
+		const status = e?.response?.status || 500
+		setResponseStatus(event, status)
+		return {
+			error: String(e)?.replace(config.tmdb.apiKey, '***'),
+		}
+	}
 })
